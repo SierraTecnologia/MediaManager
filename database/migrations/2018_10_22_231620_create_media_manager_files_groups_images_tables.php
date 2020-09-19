af65a202b49767249409bf8a320bf881e47a8cd4
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-09-19 07:00:08 -0300

    Add Github ACtions

--- database/migrations/2018_10_22_231620_create_media_manager_files_groups_images_tables.php
+++ database/migrations/2018_10_22_231620_create_media_manager_files_groups_images_tables.php
@@ -41,45 +41,6 @@ class CreateMediaManagerFilesGroupsImagesTables extends Migration
                 $table->string('imagenable_type');
             }
         );
-
-        try {
-            Schema::create(
-                'photo_albums',
-                function (Blueprint $table) {
-                    $table->engine = 'InnoDB';
-                    $table->increments('id')->unsigned();
-                    $table->integer('position')->nullable();
-                    $table->string('name', 255);
-                    $table->text('description')->nullable();
-                    $table->string('folder_id', 255);
-                    $table->unsignedInteger('user_id')->nullable();
-                    $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
-                    $table->unsignedInteger('user_id_edited')->nullable();
-                    $table->foreign('user_id_edited')->references('id')->on('users')->onDelete('set null');
-                    $table->timestamps();
-                    $table->softDeletes();
-                }
-            );
-        } catch (\Throwable $th) {
-            try {
-                Schema::create(
-                    'photo_albums',
-                    function (Blueprint $table) {
-                        $table->engine = 'InnoDB';
-                        $table->increments('id')->unsigned();
-                        $table->integer('position')->nullable();
-                        $table->string('name', 255);
-                        $table->text('description')->nullable();
-                        $table->string('folder_id', 255);
-                        $table->unsignedInteger('user_id')->nullable();
-                        $table->unsignedInteger('user_id_edited')->nullable();
-                        $table->timestamps();
-                        $table->softDeletes();
-                    }
-                );
-            } catch (\Throwable $th) {
-            }
-        }
         
 
         Schema::create(
